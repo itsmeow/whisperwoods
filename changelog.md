85dddb9cce5856688a8aba0975f41934db98f203
itsmeow <mods.itsmeow@gmail.com>
2019-10-28 16:36:08 -0400

    Fix crash redundant texture, Resolves #1, version 1.0.1, create changelog

---
+++ changelog.md
@@ -0,0 +1,3 @@
+1.0.1
+-
+- Fixed crash "Redundant texture list for particle whisperwoods:wisp"
--- src/main/java/its_meow/whisperwoods/client/init/ClientLifecycleHandler.java
+++ src/main/java/its_meow/whisperwoods/client/init/ClientLifecycleHandler.java
@@ -12,19 +12,28 @@ import its_meow.whisperwoods.entity.EntityWisp;
 import its_meow.whisperwoods.init.ModParticles;
 import its_meow.whisperwoods.tileentity.TileEntityGhostLight;
 import net.minecraft.client.Minecraft;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.ParticleFactoryRegisterEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
+@Mod.EventBusSubscriber(modid = WhisperwoodsMod.MODID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ClientLifecycleHandler {
     
     public void clientSetup(FMLClientSetupEvent event) {
         RenderingRegistry.registerEntityRenderingHandler(EntityMoth.class, RenderMoth::new);
         RenderingRegistry.registerEntityRenderingHandler(EntityHidebehind.class, RenderHidebehind::new);
         RenderingRegistry.registerEntityRenderingHandler(EntityWisp.class, RenderWisp::new);
-        Minecraft.getInstance().particles.registerFactory(ModParticles.WISP, WispParticle.WispFactory::new);
         ClientRegistry.bindTileEntitySpecialRenderer(TileEntityGhostLight.class, new RenderTileGhostLight());
         WhisperwoodsMod.LOGGER.info("Increasing wispiness of wisps...");
+    }
+    
+    @SubscribeEvent
+    public static void registerParticleFactory(ParticleFactoryRegisterEvent event) {
+        Minecraft.getInstance().particles.registerFactory(ModParticles.WISP, WispParticle.WispFactory::new);
     }
 
 }
