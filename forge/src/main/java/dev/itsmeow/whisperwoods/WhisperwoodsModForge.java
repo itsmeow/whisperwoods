d5d179e6f46809967968c65132d1b110942b51eb
itsmeow <itsmeow@itsmeow.dev>
2022-04-19 04:48:58 -0500

    Fix race condition in Forge Particle Factory registration loading

--- forge/src/main/java/dev/itsmeow/whisperwoods/WhisperwoodsModForge.java
+++ forge/src/main/java/dev/itsmeow/whisperwoods/WhisperwoodsModForge.java
@@ -1,10 +1,8 @@
 package dev.itsmeow.whisperwoods;
 
-import dev.itsmeow.whisperwoods.client.WhisperwoodsClientForge;
 import me.shedaniel.architectury.platform.forge.EventBuses;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -18,6 +16,5 @@ public class WhisperwoodsModForge {
         modBus.<FMLCommonSetupEvent>addListener(e -> {
             WhisperwoodsMod.init(e::enqueueWork);
         });
-        modBus.<FMLClientSetupEvent>addListener(e -> new WhisperwoodsClientForge().clientSetup(e));
     }
 }
--- forge/src/main/java/dev/itsmeow/whisperwoods/client/WhisperwoodsClientForge.java
+++ forge/src/main/java/dev/itsmeow/whisperwoods/client/WhisperwoodsClientForge.java
@@ -1,10 +1,24 @@
 package dev.itsmeow.whisperwoods.client;
 
+import dev.itsmeow.whisperwoods.WhisperwoodsMod;
 import dev.itsmeow.whisperwoods.client.init.ClientLifecycleHandler;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.particle.ParticleEngine;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.ParticleFactoryRegisterEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
+@Mod.EventBusSubscriber(modid = WhisperwoodsMod.MODID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class WhisperwoodsClientForge {
-    public void clientSetup(FMLClientSetupEvent event) {
+    @SubscribeEvent
+    public static void clientSetup(FMLClientSetupEvent event) {
         ClientLifecycleHandler.clientInit();
+    }
+
+    @SubscribeEvent
+    public static void registerParticleFactory(ParticleFactoryRegisterEvent event) {
+        ClientLifecycleHandler.registerParticles((type, provider) -> Minecraft.getInstance().particleEngine.register(type, (ParticleEngine.SpriteParticleRegistration) spriteSet -> provider.apply(spriteSet)));
     }
 }
